01b88bb5424c6b397941eae68398cb16edc981f6
luca <luca.carnaroli98@gmail.com>
2020-01-23 23:12:32 +0100

    prova stampa risposta pc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 $(document).ready(
   function () {
     $('.submit').click(
-      function (){
+      function gian(message){
         var message = $('.testo-messaggio').clone().addClass('msg-green');
         $('.sfondo').append(message);
-        var spunta = $('.spunta').clone().show();
-        $('.sfondo').append(spunta);
+
+        setTimeout(gian,2000);
+        function gian(){
+          var messaggioPc = 'ciao';
+          console.log(messaggioPc);
+          $(messaggioPc).clone();
+          $('.sfondo').append(messaggioPc);
+
+        }
       }
     );
+
+
     $('input').keydown(function(){ console.log(event.which);
       if (event.which == 13) {
         var message = $('.testo-messaggio').clone().addClass('msg-green');
