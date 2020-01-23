5646112086270c03683da65f20c9ae10c361dd22
luca <luca.carnaroli98@gmail.com>
2020-01-23 19:33:36 +0100

    collegato invio alla tastiera

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@ $(document).ready(
     $('.submit').click(
       function (){
         var message = $('.testo-messaggio').clone();
-        $('.sfondo').append(message);
+        $('.sfondo').append(message).addClass('.msg-green');
       }
     );
+    $('input').keydown(function(){ console.log(event.which);
+      if (event.which == 13) {
+        var message = $('.testo-messaggio').clone();
+        $('.sfondo').append(message);
+      }
+    }
+   );
   }
 );
