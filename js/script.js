ac4f353d440ba8115a1779e6d563b372ad0f064a
luca <luca.carnaroli98@gmail.com>
2020-01-23 21:59:00 +0100

    colore al msg

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ $(document).ready(
   function () {
     $('.submit').click(
       function (){
-        var message = $('.testo-messaggio').clone();
-        $('.sfondo').append(message).addClass('.msg-green');
+        var message = $('.testo-messaggio').clone().addClass('msg-green');
+        $('.sfondo').append(message);
+        var spunta = $('.spunta').clone().show();
+        $('.sfondo').append(spunta);
       }
     );
     $('input').keydown(function(){ console.log(event.which);
       if (event.which == 13) {
-        var message = $('.testo-messaggio').clone();
+        var message = $('.testo-messaggio').clone().addClass('msg-green');
         $('.sfondo').append(message);
       }
     }
